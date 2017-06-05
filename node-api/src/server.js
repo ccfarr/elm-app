33bee08aecd11c4e33d45bb2129fbf518fbd8560
ccfarr <ccfarr@gmail.com>
2017-06-05 09:01:05 -0400

    edit console.log message

--- node-api/src/server.js
+++ node-api/src/server.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response) {
-  response.send('Hello world');
+  response.send('Hello world!');
 });
 
 app.listen(8888, function() {
-  console.log('Listening on port 3000');
+  console.log('Listening on port 8888');
 });
